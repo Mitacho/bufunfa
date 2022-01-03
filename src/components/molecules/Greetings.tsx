ab615e1072efb2cc17b9aafec8a9f8076dbaf7a7
Mitacho <arthurf@tuta.io>
2022-01-03 19:13:47 -0300

    style: replace margin adding padding

--- src/components/molecules/Greetings.tsx
+++ src/components/molecules/Greetings.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, memo } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { ThemeContext } from "@styles/theme/context";
-import Button from "../atoms/button/Button";
 import Text from "@components/atoms/typography/Text";
 
 type Props = {
@@ -12,7 +11,7 @@ function Greetings({ onNavigate }: Props): JSX.Element {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <View style={[{ margin: theme.spacing.l }]}>
+    <View style={[{ paddingVertical: theme.spacing.l }]}>
       <TouchableOpacity onPress={onNavigate}>
         <Text text="Olá, Mitacho!" weight="medium" fontSize="l" />
       </TouchableOpacity>
