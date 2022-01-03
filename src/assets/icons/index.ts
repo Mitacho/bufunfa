31710276e5d5209e828c686f77ae3e96fd06bba0
Mitacho <arthurf@tuta.io>
2022-01-03 19:08:36 -0300

    chore: create icons folder

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as WalletIcon } from "./wallet";
+export { default as ReceiveMoneyIcon } from "./receiveMoney";
+export { default as CreditCardIcon } from "./creditCard";
+export { default as SavingIcon } from "./saving";
+export { default as DonateIcon } from "./donate";
