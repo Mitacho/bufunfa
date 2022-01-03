e0a094b7d8582c1088ec2f12faf12becd9239093
Mitacho <arthurf@tuta.io>
2022-01-03 19:14:43 -0300

    chore: show amount cards

--- src/components/organisms/Overview.tsx
+++ src/components/organisms/Overview.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useContext } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import BalanceCard from "@components/molecules/BalanceCard";
 import AmountCard from "@components/molecules/AmountCard";
 
 import { ThemeContext } from "@styles/theme/context";
@@ -9,10 +10,18 @@ type Props = {};
 function Overview(props: Props): JSX.Element {
   const { theme } = useContext(ThemeContext);
   return (
-    <View style={[{ marginHorizontal: theme.spacing.l }]}>
-      <AmountCard />
+    <View style={[styles.overview, { marginHorizontal: theme.spacing.l }]}>
+      <BalanceCard amount={3000} />
+      <AmountCard type="income" amount={1800.49} title="Entradas" />
+      <AmountCard type="expense" amount={1.5} title="Saídas" />
+      <AmountCard type="receive" amount={0} title="A receber" />
+      <AmountCard type="debt" amount={0} title="Dívidas" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  overview: {},
+});
 
 export default memo(Overview);
