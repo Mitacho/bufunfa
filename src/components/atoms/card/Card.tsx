5045412151adaa3abebc58c14cb07ba2812f7afa
Mitacho <arthurf@tuta.io>
2022-01-03 19:12:11 -0300

    style: adjust border and color

--- src/components/atoms/card/Card.tsx
+++ src/components/atoms/card/Card.tsx
@@ -3,21 +3,26 @@ import { View } from "react-native";
 
 import { ThemeContext } from "@styles/theme/context";
 
+import type { StyleProp, ViewStyle } from "react-native";
+
 type Props = {
   children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  color?: string;
 };
 
-function Card({ children }: Props) {
+function Card({ children, style, color }: Props) {
   const { theme } = useContext(ThemeContext);
   return (
     <View
       style={[
         {
           padding: theme.spacing.m,
-          backgroundColor: theme.colors.blue,
+          backgroundColor: color || theme.colors.blue,
           width: "100%",
-          borderRadius: 4,
+          borderRadius: 8,
         },
+        style,
       ]}>
       {children}
     </View>
