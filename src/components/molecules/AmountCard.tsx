d246874227e2ebd4703b46abc50afbbe9bb59580
Mitacho <arthurf@tuta.io>
2022-01-26 21:45:02 -0300

    chore(AmountCard): navite to screen on press event

--- src/components/molecules/AmountCard.tsx
+++ src/components/molecules/AmountCard.tsx
@@ -1,5 +1,6 @@
-import React, { memo, useContext } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { memo, useCallback, useContext } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Card } from "@components/atoms/card";
 import { Text } from "@components/atoms/typography";
@@ -7,12 +8,7 @@ import { ColoredCircleIcon } from "@components/molecules";
 
 import { ThemeContext } from "@styles/theme/context";
 
-import {
-  DonateIcon,
-  ReceiveMoneyIcon,
-  CreditCardIcon,
-  SavingIcon,
-} from "@assets/icons";
+import type { NavigationScreenProps } from "@types";
 
 type CardType = "income" | "expense" | "receive" | "debt";
 
@@ -22,17 +18,15 @@ type Props = {
   amount: number;
 };
 
-const getIcon = {
-  income: SavingIcon,
-  expense: CreditCardIcon,
-  receive: ReceiveMoneyIcon,
-  debt: DonateIcon,
-};
-
 const CIRCLE_COLOR_OPACITY = "aa";
 
 function AmountCard({ amount, title, type }: Props) {
+  const navigation = useNavigation<NavigationScreenProps<"NewTransaction">>();
   const { theme } = useContext(ThemeContext);
+
+  const handleNavigate = useCallback(() => {
+    navigation.navigate("NewTransaction");
+  }, []);
 
   const getColor = {
     income: theme.colors.cardIncome,
@@ -49,28 +43,30 @@ function AmountCard({ amount, title, type }: Props) {
   };
 
   return (
-    <Card
-      color={getColor[type]}
-      style={{ flexDirection: "row", marginVertical: theme.spacing.xs }}>
-      <View style={[styles.cardHeader, { marginRight: theme.spacing.s }]}>
-        <ColoredCircleIcon
-          icon={type}
-          circleSize={36}
-          circleColor={getCircleColor[type] + CIRCLE_COLOR_OPACITY}
-        />
-      </View>
-      <View style={[styles.amount]}>
-        <Text text={title} fontSize="m" color={theme.colors.cardText} />
-        <Text
-          text={amount}
-          currency
-          weight="medium"
-          fontSize="l"
-          color={theme.colors.cardAmount}
-          numberOfLines={1}
-        />
-      </View>
-    </Card>
+    <TouchableOpacity onPress={handleNavigate}>
+      <Card
+        color={getColor[type]}
+        style={{ flexDirection: "row", marginVertical: theme.spacing.xs }}>
+        <View style={[styles.cardHeader, { marginRight: theme.spacing.s }]}>
+          <ColoredCircleIcon
+            icon={type}
+            circleSize={36}
+            circleColor={getCircleColor[type] + CIRCLE_COLOR_OPACITY}
+          />
+        </View>
+        <View style={[styles.amount]}>
+          <Text text={title} fontSize="m" color={theme.colors.cardText} />
+          <Text
+            text={amount}
+            currency
+            weight="medium"
+            fontSize="l"
+            color={theme.colors.cardAmount}
+            numberOfLines={1}
+          />
+        </View>
+      </Card>
+    </TouchableOpacity>
   );
 }
 
