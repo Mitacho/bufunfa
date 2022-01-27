38ef8d611bfa1b90bb3c601144f2fde6fb3ce081
Mitacho <arthurf@tuta.io>
2022-01-26 21:43:04 -0300

    chore(types): create types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,18 @@
+import {
+  NativeStackScreenProps,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+
+export type Screens = "Home" | "Profile" | "NewTransaction";
 export type StackParamList = {
   Home: undefined;
   Profile: undefined;
+  NewTransaction: undefined;
 };
+
+export type ScreenProps<T extends Screens> = NativeStackScreenProps<
+  StackParamList,
+  T
+>;
+export type NavigationScreenProps<T extends Screens> =
+  NativeStackNavigationProp<StackParamList, T>;
